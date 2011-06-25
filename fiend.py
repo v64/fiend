76d8d68ee1552708a16bbd62db944c3fab1d8073
Jahn Veach <v64@v64.net>
2011-06-25 10:29:22 -0500

    v0.1

--- fiend.py
+++ fiend.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+VERSION = '0.1'
+
 import httplib2
 httplib2.debuglevel = 1
 
